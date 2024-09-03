7b4eb3cf91977907e040dc7187406a542ca51851
crutis3270 <crutis3270@gmail.com>
2024-09-03 16:26:27 -0400

    <This is a test3>

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "instance_name" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
-  default     = "This is my world now shay!"
+  default     = "This is my world now shay2!"
 }
