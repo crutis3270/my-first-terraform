b55b53d1ae35233d9589fc002e3c3f8ec524a4ff
crutis3270 <crutis3270@gmail.com>
2024-09-03 16:46:47 -0400

    <This is a test1remove>

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
-terraform {
-    backend "remote" {
-      organization = "gary-hashicorp-learn"
-      workspaces {
-        name = "Gary-Workspace"
-      }
-    }
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "2.7.0, < 4.3.0"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  region  = "us-east-1"
-}
-
+#terraform {
+#    backend "remote" {
+#      organization = "gary-hashicorp-learn"
+#      workspaces {
+#        name = "Gary-Workspace"
+#      }
+#    }
+#  required_providers {
+#    aws = {
+#      source  = "hashicorp/aws"
+#      version = "2.7.0, < 4.3.0"
+#    }
+#  }
+#
+#  required_version = ">= 1.2.0"
+#}
+#
+#provider "aws" {
+#  region  = "us-east-1"
+#}
+#
 #resource "aws_instance" "app_server" {
 #  ami           = "ami-066784287e358dad1"
 #  instance_type = "t2.micro"
