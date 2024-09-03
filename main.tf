3f3f58c4340d78dd47a0c0249ebbf75deb52203a
crutis3270 <crutis3270@gmail.com>
2024-09-03 16:44:22 -0400

    <This is a test5remove>

--- main.tf
+++ main.tf
@@ -19,11 +19,11 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-066784287e358dad1"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = var.instance_name
-  }
-}
+#resource "aws_instance" "app_server" {
+#  ami           = "ami-066784287e358dad1"
+#  instance_type = "t2.micro"
+#
+#  tags = {
+#    Name = var.instance_name
+#  }
+#}
